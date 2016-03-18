77c59a6c069a0d63e1cdd1fdc2cba75d067d2ec6
Raphael Trzpit <rtrzpit@gmail.com>
2016-03-18 16:35:30 +0100

    init behat

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Majora\Bundle\FrameworkExtraBundle\MajoraFrameworkExtraBundle($this),
+            new Acme\Lv\Bundle\ApiBundle\AcmeLvApiBundle(),
+            new Acme\Lv\Bundle\DalBundle\AcmeLvDalBundle(),
+            new Acme\Lv\Bundle\SdkBundle\AcmeLvSdkBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
