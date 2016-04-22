53a5f15dc0a11aba21ed75e48859770bdac1fdb0
Raphael Trzpit <rtrzpit@gmail.com>
2016-04-22 16:57:34 +0200

    Refacto WIP

--- skeletons/MajoraVendor/MajoraNamespace/Bundle/DalBundle/MajoraVendorMajoraNamespaceDalBundle.php
+++ skeletons/MajoraVendor/MajoraNamespace/Bundle/DalBundle/MajoraVendorMajoraNamespaceDalBundle.php
@@ -5,7 +5,7 @@ namespace MajoraVendor\MajoraNamespace\Bundle\DalBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
- * @MajoraGenerator({"register_bundle": {"target": "\\ApiAppKernel"}})
+ * @MajoraGenerator({"register_bundle": {"target": "\\AppKernel"}})
  */
 class MajoraVendorMajoraNamespaceDalBundle extends Bundle
 {
