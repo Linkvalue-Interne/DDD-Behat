9f5381fc62d5ec90ff60cf6727542aa9c576e1ef
Raphael Trzpit <rtrzpit@gmail.com>
2016-04-22 11:56:46 +0200

    Add better implementation of fixtures

--- src/Acme/Lv/Bundle/ApiBundle/Features/Context/PostContext.php
+++ src/Acme/Lv/Bundle/ApiBundle/Features/Context/PostContext.php
@@ -6,36 +6,81 @@ use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\TableNode;
 use Acme\Lv\Component\Entity\Post;
 use Acme\Lv\Component\Entity\PostCollection;
-use Acme\Lv\Component\Repository\PostRepositoryInterface;
 use Acme\Lv\Component\Loader\PostLoaderInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * Defines application features from the specific context.
  */
 class PostContext implements Context
 {
-    /**
-     * @var PostCollection
-     */
-    protected $posts;
-
-    /**
-     * @var PostRepositoryInterface
-     */
-    protected $repository;
 
     /**
      * @var PostLoaderInterface
      */
     protected $loader;
 
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
+
+    /**
+     * @var PostCollection
+     */
+    protected $posts;
+
     public function __construct(
-        PostRepositoryInterface $repository,
+        EntityManagerInterface $em,
         PostLoaderInterface $loader)
     {
-        $this->repository = $repository;
+        $this->em = $em;
         $this->loader = $loader;
+
         $this->posts = new PostCollection();
+    }
+
+    /**
+     * @BeforeSuite
+     */
+    public static function initDabatabase()
+    {
+        exec('php app/console --env=test doctrine:schema:drop --force');
+        exec('php app/console --env=test doctrine:schema:create');
+        // No use of fixtures, loaded in background.
+        // exec('php app/console --env=test doctrine:fixtures:load --no-interaction');
+        echo 'Bdd initialized';
+    }
+
+    /**
+     * @BeforeScenario @Post
+     */
+    public function BeforeScenarioPost()
+    {
+        $this->em->getConnection();
+        $this->em->beginTransaction();
+    }
+
+    /**
+     * @AfterScenario @Post
+     */
+    public function AfterScenarioPost()
+    {
+        $this->em->rollback();
+        $this->em->close();
+    }
+
+    /**
+     * @Given I have theses posts:
+     */
+    public function iHaveThesesPosts(PostCollection $posts)
+    {
+        // Fill posts table.
+        foreach ($posts as $post) {
+            $this->em->persist($post);
+        }
+        $this->em->flush();
+        $this->posts = $posts;
     }
 
     /**
@@ -56,38 +101,6 @@ class PostContext implements Context
         }
 
         return $posts;
-    }
-
-    /**
-     * @Given I have theses posts:
-     */
-    public function iHaveThesesPosts(PostCollection $posts)
-    {
-        // Flush posts table.
-        $oldPosts = $this->loader->retrieveAll();
-        foreach ($oldPosts as $oldPost) {
-            $this->repository->remove($oldPost);
-        }
-
-        // Fill posts table.
-        foreach ($posts as $post) {
-            $this->repository->persist($post);
-        }
-        $this->posts = $posts;
-    }
-
-    /**
-     * Bootstrap test database.
-     *
-     * @BeforeFeature
-     */
-    public static function initBdd()
-    {
-        exec('php app/console --env=test doctrine:schema:drop --force');
-        exec('php app/console --env=test doctrine:schema:create');
-        // No use of fixtures, loaded in background.
-        // exec('php app/console --env=test doctrine:fixtures:load --no-interaction');
-        echo 'Bdd initialized';
     }
 
     /**
