19afacd6375ec5d5384ba11b03d588bde7d71d58
Gael Millet <gmillet@m6web.fr>
2016-07-29 16:44:17 +0200

    Move behat.yml and add annotation for MajoraGenerator

--- skeletons/MajoraVendor/MajoraNamespace/Bundle/ApiBundle/MajoraVendorMajoraNamespaceApiBundle.php
+++ skeletons/MajoraVendor/MajoraNamespace/Bundle/ApiBundle/MajoraVendorMajoraNamespaceApiBundle.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * @MajoraGenerator({"register_bundle": {"target": "\\AppKernel"}})
+ * @MajoraGenerator({"register_behat": {"target": "/config/behat.yml"}})
  */
 class MajoraVendorMajoraNamespaceApiBundle extends Bundle
 {
